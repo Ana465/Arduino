0a23f00c71d08ddb1cc5877d6ab0c0be7a5dbcad
Ana465 <40030373+Ana465@users.noreply.github.com>
2020-02-03 18:04:30 -0300

    Add files via upload

---
+++ interrupt.ino
@@ -0,0 +1,32 @@
+int interrupcaoB1 = 0;
+int buzzer = 4;
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  attachInterrupt(interrupcaoB1,frequencia, RISING);
+  pinMode(buzzer, OUTPUT);
+  
+  
+}
+
+void loop() 
+{
+  while(true)
+ {
+  Serial.print(".");
+  delay(250);
+ }
+ noTone(buzzer);
+
+}
+
+
+void frequencia()
+{
+  tone(buzzer, 1000);
+  Serial.print("+");
+  
+}
+
